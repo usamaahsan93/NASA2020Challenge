32bc877e7b74685e6ff251621f7dc4fc4f1bcd45
Rawan Omar Gaafar <40496489+RaOneG@users.noreply.github.com>
2020-05-31 23:19:05 +0200

    Update GUI.py

--- Code/GUI.py
+++ Code/GUI.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter.ttk import *
 import numpy as np
 import matplotlib
+import matplotlib.pyplot
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -52,12 +53,11 @@ def main():
     # and create data frames to store the data
     df = pd.read_csv(chosen_country+'.csv')
     dates = df['date']
-    #dates = [datetime.datetime.strptime(d, "%d-%m-%Y").date() for d in dates]
     dates = np.array(dates)
-    df = df.drop(columns=['Date_repor', 'Country', 'WHO_region', 'date', 'tests_units'])
+    df = df.drop(columns=['Country', 'WHO_region', 'date', 'tests_units'])
 
     # scale the data frames
-    scaling = RobustScaler()
+    scaling = MinMaxScaler()
     scaled = scaling.fit_transform(df)
     df_scaled = pd.DataFrame(scaled, columns=df.columns, index=df.index)
 
@@ -68,68 +68,69 @@ def main():
     # plot the cases vs. the dates
     cases_axis.plot(df_scaled['total_cases'], '*', label='Total Cases')
 
-    cases_axis.plot(df_scaled['Close public transport (OxBSG)'], 'o', label='Public Transport')
-
-    cases_axis.plot(df_scaled['International travel controls (OxBSG)'], 'h', label='International Travel Controls')
-
-    cases_axis.plot(df_scaled['Workplace Closures (OxBSG)'], 's', label='Workplace Closures')
-
-    cases_axis.plot(df_scaled['Cancel public events (OxBSG)'], '1', label='Cancel public events')
-
-    cases_axis.plot(df_scaled['Restrictions on gatherings (OxBSG)'], 'd', label='Restrictions on gatherings')
-
-    cases_axis.plot(df_scaled['Stay at home requirements (OxBSG)'], 'D', label='Stay at home requirements')
-
-    cases_axis.plot(df_scaled['Restrictions on internal movement (OxBSG)'], 'x', label='Restrictions on internal movement')
-
     xx = np.arange(0, 120, 10)
     x_dates = dates[xx]
-    cases_axis.set_xticks(xx)
-    cases_axis.set_xticks(x_dates)
+    cases_axis.set_xticks(xx, np.all(x_dates))
 
     cases_axis.set_xlabel('Time', fontsize=14)
     cases_axis.set_title(chosen_country, fontsize=18)
     cases_axis.legend()
     cases_axis.grid()
 
+    def transport_clicked():
+        # show transport curve
+        cases_axis.plot(df_scaled['Close public transport (OxBSG)'], 'o', label='Public Transport')
+
+    def event_clicked():
+        # show public events cancelation curve
+        cases_axis.plot(df_scaled['Cancel public events (OxBSG)'], '1', label='Cancel public events')
+
+    def travel_clicked():
+        # show international travel curve curve
+        cases_axis.plot(df_scaled['International travel controls (OxBSG)'], 'h', label='International Travel Controls')
+
+    def workplace_clicked():
+        # show workplace closure curve
+        cases_axis.plot(df_scaled['Workplace Closures (OxBSG)'], 's', label='Workplace Closures')
+
+    def gathering_clicked():
+        # show restriction on gathering control's curve
+        cases_axis.plot(df_scaled['Restrictions on gatherings (OxBSG)'], 'd', label='Restrictions on gatherings')
+
+    def internal_movement_clicked():
+        # show internal movement restriction curve
+        cases_axis.plot(df_scaled['Restrictions on internal movement (OxBSG)'], 'x',
+                        label='Restrictions on internal movement')
+
+    def stay_home_clicked():
+        # show stay at home curve
+        cases_axis.plot(df_scaled['Stay at home requirements (OxBSG)'], 'D', label='Stay at home requirements')
+
     transport_button = Button(window, text="Public Transport", command=transport_clicked)
     transport_button.place(anchor=NW, rely=0.1)
 
-    events_button = Button(window, text="Event Cancel", command=event_clicked)
+    events_button = Button(window, text="Cancel public events", command=event_clicked)
     events_button.place(anchor=NW, rely=0.2)
 
-    school_button = Button(window, text="School Lock-down", command=school_clicked)
-    school_button.place(anchor=NW, rely=0.3)
+    travel_button = Button(window, text="International Travel Controls", command=travel_clicked)
+    travel_button.place(anchor=NW, rely=0.3)
 
-    workplace_button = Button(window, text="Workplace Lock-down", command=workplace_clicked)
+    workplace_button = Button(window, text="Workplace Closures", command=workplace_clicked)
     workplace_button.place(anchor=NW, rely=0.4)
+
+    gathering_button = Button(window, text="Cancel public events", command=gathering_clicked)
+    gathering_button.place(anchor=NW, rely=0.5)
+
+    internal_movement_button = Button(window, text="Restrictions on internal movement", command=internal_movement_clicked)
+    internal_movement_button.place(anchor=NW, rely=0.6)
+
+    stay_home_button = Button(window, text="Stay at home requirements", command=stay_home_clicked)
+    stay_home_button.place(anchor=NW, rely=0.7)
 
     cases_graph = FigureCanvasTkAgg(cases_figure, window)
     cases_graph.get_tk_widget().pack(side=RIGHT, fill=BOTH)
 
     window.mainloop()
-
-
-def transport_clicked():
-    # show transport curve
-    #cases_axis.plot(df_scaled['Close public transport (OxBSG)'], label='Public Transport')
-    print("hi")
-
-
-def event_clicked():
-    # show international travel control's curve
-    #cases_axis.plot(df_scaled['International travel controls (OxBSG)'], label='International Travel Controls')
-    print("hi")
-
-
-def school_clicked():
-    # show transport curve
-    print("School curve")
-
-
-def workplace_clicked():
-    # show transport curve
-    print("workplace curve")
 
 
 def make_window(title):
